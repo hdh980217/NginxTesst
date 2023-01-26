f9b4089f01ce13ea8a83b373a0d8a36abacc504b
hyeon <hyeon980217@gmail.com>
2023-01-26 10:46:53 +0900

    간단한 axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,69 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useRef, useState } from 'react';
+import './App.css'
 
 const App = () => {
+  const idRef = useRef();
+  const nameRef = useRef();
+  const [temp, setTemp] = useState();
+
+  const insert = () => {
+    console.log(idRef.current.value, nameRef.current.value);
+    const id = idRef.current.value;
+    const name = nameRef.current.value;
+    idRef.current.value = '';
+    nameRef.current.value = '';
+
+    axios.post('/api', {id : id, name : name})
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  const select = () => {
+    axios.get('/api', {})
+        .then((res) => {
+            console.log(res.data);
+            let result = [];
+            for (let i = 0; i < res.data.length; i++) {
+                const data = res.data[i];
+                result.push(
+                    <tr key={i+1}>
+                        <td>{data.id}</td>		
+                        <td>{data.name}</td>		
+                    </tr>
+                );
+            }
+            setTemp(result);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+  }
+
   return (
     <div>
       <h1>App</h1>
+      <input ref={idRef} type='text' placeholder='id'/>
+      <input ref={nameRef} type='text' placeholder='name'/>
+      <button onClick={insert}>insert</button><br/><br/>
+
+      <button onClick={select}>select</button><br/><br/>
+      <table border='1'>
+        <thead>
+          <tr>
+            <td>id</td>
+            <td>name</td>
+          </tr>
+        </thead>
+        <tbody>
+          {temp}
+        </tbody>
+      </table>
+
     </div>
   );
 };
